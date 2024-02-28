5c0c2958ba37a61005055082e7639116ec1e83c0
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-02-28 20:58:32 -0300

    fix: banner directory path

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -6,7 +6,7 @@ export default function Banner({url}: props) {
   return (
     <div className="w-full max-h-[20rem] overflow-hidden mt-14 flex items-center justify-center">
       <img
-        src={`/public/${url}.webp`}
+        src={`public/${url}.webp`}
         alt="banner do filme"
         className="w-full h-full object-cover"
       />
