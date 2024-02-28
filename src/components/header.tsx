8aa08d57e8231d58af0ce2b3c4e85bd866ac1df1
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-02-27 22:31:06 -0300

    style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="w-full h-16 border-b flex items-center justify-around">
             <nav className="flex items-center gap-10">
-                <span className="text-2xl font-bold">cine maze</span>
+                <span className="text-2xl font-bold">cinemaze</span>
                 <div className="flex items-center gap-4">
                 <a href="#">Início</a>
                 <a href="#">Assistir</a>
