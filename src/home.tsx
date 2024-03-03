eb8b60ea31f1f0af6ca4d8cf1871ee89a99c410a
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-03-02 23:47:56 -0300

    feat: add search input

--- src/home.tsx
+++ src/home.tsx
@@ -3,12 +3,30 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Card from "@/components/card";
+import { Input } from "./components/ui/input";
+import { useState } from "react";
 
 function Home() {
+  const [name, setName] = useState("")
+
+  function getName(e: React.ChangeEvent<HTMLInputElement>) {
+     setName(e.target.value)  
+  }
+
+
   return (
     <main className="w-full flex flex-col items-center">
       <Header />
       <Hero>
+
+        <Input
+        type="text"
+        placeholder="Nome"
+        onChange={getName}
+        className="mt-20 max-w-72"
+        />
+        <h2 className="text-primary mt-3">{name}</h2>
+
         {categories.map((category, index) => (
           <Category key={index} category={category}>
             {filterCategory(index).map(({ id }) => {
